0f52eddb4a0973dab3143881d8c1854659b7779a
Hugh Rawlinson <hugh@spotify.com>
2017-05-20 17:08:30 +0200

    Initial Commit

---
+++ mediakeys-spotify-connect/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  mediakeys-spotify-connect
+//
+//  Created by Hugh Rawlinson on 2017-05-20.
+//  Copyright © 2017 Hugh Rawlinson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
